b845f0d648e00801084ce4f18449f51f1303d42a
heyallnorahere <nora.github@bedafamily.com>
2025-09-06 23:05:40 -0700

    signal visualizer

--- managed/Schmix.Example/Output.cs
+++ managed/Schmix.Example/Output.cs
@@ -1,5 +1,7 @@
 namespace Schmix.Example;
 
+using ImGuiNET;
+
 using Schmix.Audio;
 using Schmix.Extension;
 using Schmix.UI;
@@ -13,12 +15,16 @@ internal sealed class OutputModule : Module
     {
         uint id = OutputDevice.Default;
         mOutput = new OutputDevice(id, Rack.SampleRate, Rack.Channels);
+
+        mDisplayedSignal = null;
     }
 
     protected override void Cleanup(bool disposed)
     {
         mOutput.Dispose();
     }
+
+    public int ChunkSize => mOutput.SampleRate / 30;
 
     public override int SamplesRequested
     {
@@ -30,7 +36,7 @@ internal sealed class OutputModule : Module
             }
 
             int queued = mOutput.QueuedSamples;
-            int chunkSize = mOutput.SampleRate / 30;
+            int chunkSize = ChunkSize;
 
             if (queued < mOutput.SampleRate / 4)
             {
@@ -47,23 +53,41 @@ internal sealed class OutputModule : Module
 
     public override string GetInputName(int index) => index > 0 ? "<unused>" : "Audio";
 
+    public override void DrawProperties()
+    {
+        ImGui.PushItemWidth(150f);
+
+        int channels = mOutput.Channels;
+        int chunkSize = ChunkSize;
+
+        for (int i = 0; i < channels; i++)
+        {
+            var samples = new float[chunkSize];
+            for (int j = 0; j < chunkSize; j++)
+            {
+                samples[j] = (float)(mDisplayedSignal?[i][j] ?? 0);
+            }
+
+            ImGui.PlotLines($"##channel-{i}", ref samples[0], chunkSize, 0, $"Channel {i + 1}", -1f, 1f, System.Numerics.Vector2.UnitY * 80f);
+        }
+
+        ImGui.PopItemWidth();
+    }
+
     public override void Process(IReadOnlyList<ISignalInput?> inputs, IReadOnlyList<ISignalOutput?> outputs, int sampleRate, int samplesRequested, int channels)
     {
         var audioInput = inputs[0];
         var audio = audioInput?.Signal;
 
-        if (audio is null)
-        {
-            return;
-        }
-
-        if (!mOutput.PutAudio(audio))
+        mDisplayedSignal = audio?.Copy();
+        if (audio is not null && !mOutput.PutAudio(audio))
         {
             throw new InvalidOperationException("Failed to send audio to output device!");
         }
     }
 
     private OutputDevice mOutput;
+    private StereoSignal<double>? mDisplayedSignal;
 }
 
 [RegisteredPlugin("Output")]
